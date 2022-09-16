60d0ab9b30a58178e5166e7b25a73f6a82be245a
Decode-Chan <70747479+decodexuwu@users.noreply.github.com>
2022-09-16 12:50:30 -0400

    Major Update

---
+++ src/script/index.js
@@ -0,0 +1,6 @@
+function switchImg() {
+    document.querySelector(`.lab-100`).classList.toggle("active");
+    document.querySelector(`.lab-100`).classList.toggle("inactive");
+    document.querySelector(`.lab-200`).classList.toggle("active");
+    document.querySelector(`.lab-200`).classList.toggle("inactive");
+}
