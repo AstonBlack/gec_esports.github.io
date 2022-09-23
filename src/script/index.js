406432e50f33cb149a774d157804b4f5c549d247
Decode-Chan <70747479+decodexuwu@users.noreply.github.com>
2022-09-23 16:01:56 -0400

    Update 2

--- src/script/index.js
+++ src/script/index.js
@@ -4,3 +4,11 @@ function switchImg() {
     document.querySelector(`.lab-200`).classList.toggle("active");
     document.querySelector(`.lab-200`).classList.toggle("inactive");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.querySelector(".lds-ripple").style.opacity = 0;
+        document.querySelector(".loaderText").innerHTML = "No competitions found. "
+        document.querySelector(".loaderText").classList.add('active');
+    }, 1800)
+})
